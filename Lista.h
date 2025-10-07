127396866777a4b260aabbcdca774f33096ef906
Marcofati <mdv00011@red.ujaen.es>
2025-10-07 15:59:35 +0200

    Lista.h

--- Lista.h
+++ Lista.h
@@ -17,39 +17,50 @@ private:
     public:
         N dato;
         Nodo *sig;
-        Nodo(N &dato, Nodo *sig=0);
+        Nodo(N &_dato, Nodo *_sig=0):
+        dato(_dato), sig(_sig)
+        {}
+        ~Nodo(){}
     }; //Fin clase nodo
     Nodo<L> *cabecera, *cola;
     int tam=0;
-
+    void limpia_lista();
 public:
     template<typename I>
     class Iterador {
         Nodo<I> *nodo;
     public:
-        Iterador(Nodo<I> *_nodo): nodo (_nodo){}
+        friend class ListaEnlazada<I>;
+        Iterador(Nodo<I> *_nodo=0): nodo (_nodo){}
         bool fin() {
             return nodo==0;
         }
         void siguiente() {
             nodo = nodo->sig;
         }
-        I &dato(){ return nodo->dato; }
-        I &operator*(){ return nodo->dato; }
+        Nodo<L> *get_nodo() const {return nodo;}
+        L &dato(){ return nodo->dato; }
+        L &operator*(){ return nodo->dato; }
+        ~Iterador(){}
     }; //Fin clase iterador
 
     ListaEnlazada() : cabecera(0), cola(0), tam(0) {}
-    ~ListaEnlazada(){}
-    ListaEnlazada(const ListaEnlazada &origen);
-    ListaEnlazada &operator=(const ListaEnlazada &origen);
-    Iterador<L> iterador() { return Iterador<L>(cabecera);}
+    ~ListaEnlazada();
+    ListaEnlazada(const ListaEnlazada<L> &origen);
+    ListaEnlazada &operator=(const ListaEnlazada<L> &origen);
+    Iterador<L> iterador() {return Iterador<L>(cabecera);}
     void insertarinicio(L &dato);
     void insertarFinal(L &dato);
-    void insertar(Iterator<L> &i, L &dato);
+    void insertar_delante(Iterador &i, const L &dato);
+    void insertarDetras(Iterador<L> &i, const L &dato);
     void borrarInicio();
-    void borrarFinal(Iterador<L> &i);
+    void borrarFinal();
+    void borrar(Iterador &i);
+    int get_tam(){return tam;}
     L &inicio();
     L &final();
+    ListaEnlazada<L> concatena(const ListaEnlazada<L> &origen);
+    ListaEnlazada<L> operator+(const ListaEnlazada<L> &origen);
 };
 
 template<typename L>
@@ -63,7 +74,7 @@ template<typename L>
 ListaEnlazada<L> &ListaEnlazada<L>::operator=(const ListaEnlazada<L> &origen) {
     if(cabecera != &origen.cabecera){
         while(cabecera != 0){
-            Nodo<T> aux = cabecera;
+            Nodo<L> *aux = cabecera;
             cabecera = cabecera->sig;
             delete aux;
         }
@@ -86,6 +97,9 @@ ListaEnlazada<L> &ListaEnlazada<L>::operator=(const ListaEnlazada<L> &origen) {
  */
 template<typename L>
 L &ListaEnlazada<L>::inicio() {
+    if(cabecera==nullptr) {
+        throw std::invalid_argument("Cabecera no existe");
+    }
     Iterador<L> i= cabecera;
     return i.dato();
 }
@@ -94,7 +108,10 @@ L &ListaEnlazada<L>::inicio() {
  * @return Dato al final de la lista
  */
 template<typename L>
-L &ListaEnlazada<L>::fin() {
+L &ListaEnlazada<L>::final() {
+    if(cola==nullptr) {
+        throw std::invalid_argument("Cola no existe");
+    }
     Iterador<L> i= cola;
     return i.dato();
 }
@@ -105,7 +122,7 @@ L &ListaEnlazada<L>::fin() {
  * @post La funcion añade un nuevo dato al comienzo de la lista, sustituyendo al que en ese momento sea la cabecera. Si no hubiera un dato, este seria tambien la cola
  */
 template<typename L>
-void ListaEnlazada<L>::insertaInicio(L &dato) {
+void ListaEnlazada<L>::insertarinicio(L &dato) {
     Nodo<L> *nuevo = new Nodo<L>(dato, cabecera); //aqui lo que hago es crear el nuevo nodo y ademas hago que apunte al actual primero
 
     if (cabecera == 0) {
@@ -122,7 +139,7 @@ void ListaEnlazada<L>::insertaInicio(L &dato) {
  * @post La funcion añade un nuevo dato al final de la lista, sustituyendo al que en ese momento sea la cola. Si no hubiera un dato, este seria tambien la cabecera
  */
 template<typename L>
-void ListaEnlazada<L>::insertaFin(L &dato) {
+void ListaEnlazada<L>::insertarFinal(L &dato) {
     Nodo<L> *nuevo = new Nodo<L>(dato, 0); //aqui lo que hago es crear el nuevo nodo y ademas hago que apunte al actual ultimo
     if (cola != 0) { //Si la lista no esta vacia, el siguiente a el ultimo es el nuevo
         cola->sig = nuevo;
@@ -136,9 +153,32 @@ void ListaEnlazada<L>::insertaFin(L &dato) {
 }
 
 template<typename L>
-void ListaEnlazada<L>::insertaDetras(Iterador &i, L &dato) {
+void ListaEnlazada<L>::insertarDetras(Iterador &i, L &dato) {
+    Nodo<L> *nuevo = new Nodo<L>(dato, i->sig);
+    i->sig=nuevo;
+    if(cola==i) {
+        cola=nuevo;
+    }
+    tam++;
+}
+template<typename L>
+void ListaEnlazada<L>::insertar_delante(Iterador &i, const L &dato) {
     Nodo<L> *nuevo = new Nodo<L>(dato, i);
-
+    if(i==cabecera) {
+        cabecera = nuevo;
+    }
+    Nodo<L> *anterior= 0;
+    if(cabecera!=cola) {
+        anterior=cabecera;
+        while(anterior->sig !=i) {
+            anterior=anterior->sig;
+        }
+        anterior->sig=nuevo;
+    }
+    if(cabecera==0) {
+        cola=cabecera=nuevo;
+    }
+    tam++;
 }
 
 /**
@@ -147,7 +187,7 @@ void ListaEnlazada<L>::insertaDetras(Iterador &i, L &dato) {
  * @throw Se lanza una excepcion si se intenta eliminar un nodo de una lista vacia
  */
 template<typename L>
-void ListaEnlazada<L>::borrarInicio() {
+void ListaEnlazada<L>::borrarInicio(){
     if (cabecera == 0) {
        throw std::invalid_argument("Error al borrar por el principio, la lista está vacia");
     }
@@ -160,6 +200,79 @@ void ListaEnlazada<L>::borrarInicio() {
     }
     tam--;
 }
+/**
+ * @brief Metodo que elimina el último nodo de la lista
+ * @post El ultimo nodo de la lista es borrado, en caso de no haber nada cabecera y cola apuntan a 0
+ */
+template<typename L>
+void ListaEnlazada<L>::borrarFinal(){
+    Nodo<L> *borrado = 0;
+    if(cabecera != cola) {
+        borrado=cabecera;
+        while(borrado->sig != cola) {
+            borrado=borrado->sig;
+        }
+    }
+    delete cola;
+    cola=borrado;
+    if(borrado != 0) {
+        borrado->sig=0;
+    }else {
+        cabecera=0;
+    }
+    tam--;
+}
+
+template<typename L>
+void ListaEnlazada<L>::borrar(Iterador &i) {
+    Nodo<L> *borrado = 0;
+    if(cabecera != cola) {
+        borrado=cabecera;
+        while(borrado->sig != i) {
+            borrado=borrado->sig;
+        }
+    }
+    borrado->sig=i->sig;
+    delete i;
+    tam--;
+}
+template<typename L>
+ListaEnlazada<L>::~ListaEnlazada() {
+    while(cabecera != 0){
+        Nodo<L> *aux = cabecera;
+        cabecera = cabecera->sig;
+        delete aux;
+    }
+}
+
+template<typename L>
+void ListaEnlazada<L>::limpia_lista() {
+    if(cabecera) {
+        throw std::invalid_argument("No hay nada que limpiar");
+    }
+        while(cabecera != 0){
+            Nodo<L> *aux = cabecera;
+            cabecera = cabecera->sig;
+            delete aux;
+    }
+}
+template<typename L>
+ListaEnlazada<L> ListaEnlazada<L>::concatena(const ListaEnlazada<L> &origen) {
+    ListaEnlazada<L> nueva;
+    Nodo<L> *aux = cabecera;
+    while(aux->sig != 0) {
+        nueva.insertarFinal(aux->dato);
+        aux=aux->sig;
+    }
+    aux=origen.cabecera;
+    while(aux->sig != 0) {
+        nueva.insertarFinal(aux->dato);
+        aux=aux->sig;
+    }
+    return nueva;
+}
+
+
 
 
 
